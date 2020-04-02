3291d019c244649758dfbf2bc25ec24a6ea0d834
Bao PT <phutuongbao1999@gmail.com>
2020-04-02 17:53:02 +0700

    calculate Mortgage

--- src/com/baodev/Main.java
+++ src/com/baodev/Main.java
@@ -1,43 +1,33 @@
 package com.baodev;
 
 import java.text.NumberFormat;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        long viewCount = 3_123_456_789L;
-        float price = 10.99F;
-        char letter = 'A';
-        boolean isKnowJava = false;
-        // This Is For Decalre Primitive Variables
-        Date now = new Date();
-        // now is for decalre Refrence Variables
-        String message = "Hello This Is \"String\"";
-        String messageChange = message.replace("String", "Ahihi");
-        System.out.println(message);
-        System.out.println(messageChange);
-        // This is String in Java
-        int[] numbers = { 1, 3, 2, 6, 5 };
-        Arrays.sort(numbers);
-        System.out.println(numbers.length);
-        System.out.println(Arrays.toString(numbers));
-        int[][] numbers2 = { { 1, 2, 3 }, { 4, 5, 6 } };
-        System.out.println(Arrays.deepToString(numbers2));
-        // Arrays
-        final float PI = 3.14F;
-        // Cannot Change Value of PI
-        // Const Variables
-        double result = (double)10 / (double)3;
-        System.out.println(result);
-        // Math Operator
-        int math = (int) (Math.random() * 100);
-        System.out.println(math);
-        // Math Class
-        String result2 = NumberFormat.getPercentInstance().format(0.1);
-        System.out.println(result2);
-        // Math Formatting
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Principal: ");
+        int principal = scanner.nextInt();
+
+        System.out.print("Annual Interest Rate: ");
+        float annualInterest = scanner.nextFloat();
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+
+        System.out.println("Period (Years): ");
+        byte years = scanner.nextByte();
+        int numberOfPayments = years * MONTHS_IN_YEAR;
+
+        double mortgage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormatted
+        );
     }
 }
